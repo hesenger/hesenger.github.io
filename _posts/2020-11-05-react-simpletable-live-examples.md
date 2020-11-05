34812f5161fb0646aa3abd7c66537ee56cd29198
Heber E. Senger <sengerhe@gmail.com>
2020-11-05 18:34:33 -0300

    deploy examples simpletable

---
+++ _posts/2020-11-05-react-simpletable-live-examples.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "React SimpleTable live examples"
+date: 2020-11-05 18:28:42 -0300
+categories: blog
+---
+
+I'm published the examples directory to work here, so anyone is able to
+view the reac-simpletable running in any device.
+
+[Click here to see SimpleTable working](/react-simpletable).
---
+++ react-simpletable/precache-manifest.41fc77ef768921c46b1b01cce50f4d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e383dcb45aae66e126b76aaa7e4fc260",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c25299ead0eeda90e1ff",
+    "url": "./static/css/main.eb0ff1f7.chunk.css"
+  },
+  {
+    "revision": "e9846e287456f7d4cb86",
+    "url": "./static/js/2.bf291459.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bf291459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25299ead0eeda90e1ff",
+    "url": "./static/js/main.edcb6e0e.chunk.js"
+  },
+  {
+    "revision": "9e351750b48491bf4a87",
+    "url": "./static/js/runtime-main.bca94345.js"
+  }
+]);
